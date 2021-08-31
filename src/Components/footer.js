25523f95321ba76254f4c10096737f9056b2675e
Anubhab Patnaik <anubhabr50@gmail.com>
2021-08-31 16:34:38 +0530

    footerupdates

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -7,16 +7,15 @@ import { Card, Button} from 'react-bootstrap'
 export const Footer = () => {
 	return (
 		<div>
-			<Card className="text-center">
-				<Card.Header>Featured</Card.Header>
+			<Card className="text-center bg-dark">
 				<Card.Body>
 					<Card.Title>Special title treatment</Card.Title>
-					<Card.Text>
+					<Card.Text className="text-white">
 						With supporting text below as a natural lead-in to additional content.
 					</Card.Text>
-					<Button variant="primary">Go somewhere</Button>
+					<Button variant="secondary">Github</Button>
 				</Card.Body>
-				<Card.Footer className="text-muted">2 days ago</Card.Footer>
+				<Card.Footer className="text-muted">Copyright Anubhab Patnaik -@github.com/codebotx</Card.Footer>
 			</Card>
 		</div>
 	)
--- src/Components/todos.js
+++ src/Components/todos.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Todo } from './todo'
-
+import { Card } from 'react-bootstrap'
 
 export const Todos = (props) => {
 	return (
 		<div className="container">
 			<h3 className="text-center my-3">Todos List</h3>
-			{props.todos.length === 0 ? <div className="text-center">
-				Congrats! You have succesfully completed all your tasks</div> :
+			{props.todos.length === 0 ? <Card><div className="text-center">
+				Congrats! You have succesfully completed all your tasks</div> </Card>:
 				props.todos.map((todo) => {
 					return <Todo todo={todo} key={todo.sno} onDelete={props.onDelete} />
 				})
