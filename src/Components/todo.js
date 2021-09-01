61b12e6ba67a60e7d1c0dd5dc215026757899946
Anubhab Patnaik <anubhabr50@gmail.com>
2021-09-01 09:44:18 +0530

    todo items final commit

--- src/Components/todo.js
+++ src/Components/todo.js
@@ -3,10 +3,13 @@ import React from 'react'
 export const Todo = ({ todo,onDelete }) => {
 	// console.log(todo)
 	return (
+		<>
 		<div>
 			<h4>{todo.title}</h4>
 			<p>{todo.desc}</p>
 			<button className="btn btn-sm btn-danger"onClick={()=>onDelete(todo)}>Delete</button>
 		</div>
+		<hr/>
+		</>
 	)
 }
