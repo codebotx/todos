fdec5fc2fc752cea12d0b93b9aac672fa4dac896
Anubhab Patnaik <anubhabr50@gmail.com>
2021-09-01 09:47:48 +0530

    todos final commit

--- src/Components/todos.js
+++ src/Components/todos.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import { Todo } from './todo'
-import { Card } from 'react-bootstrap'
+
 
 export const Todos = (props) => {
 	let myStyle ={
-		minHeight: "100vh"
+		minHeight: "70vh",
+		margin:"40px auto"
 	}
 	return (
 		<div className="container my-3" style={myStyle}>
 			<h3 className="text-center my-3">Todos List</h3>
-			{props.todos.length === 0 ? <Card><div className="text-center">
-				Congrats! You have succesfully completed all your tasks</div> </Card>:
+			{props.todos.length === 0 ? <div className="text-center">
+				No Todos to display. You have succesfully completed all your tasks!</div>:
 				props.todos.map((todo) => {
-					return <Todo todo={todo} key={todo.sno} onDelete={props.onDelete} />
-				})
+					console.log(todo.sno);
+					return(<Todo todo={todo} key={todo.sno} onDelete={props.onDelete} />)
+			})
 			}
 		</div>
 	)
