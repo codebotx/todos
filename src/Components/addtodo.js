ee2c06b5d913972b4730f78142c2492dc314223d
Anubhab Patnaik <anubhabr50@gmail.com>
2021-09-01 09:38:03 +0530

    Adding todo files

--- src/Components/addtodo.js
+++ src/Components/addtodo.js
@@ -4,16 +4,21 @@ import { useState } from 'react'
 
 export const Addtodo = (props) => {
 
-	const[title, setTitle]=useState("");
-	const[desc, setDesc]=useState("");
+	const [title, setTitle] = useState("");
+	const [desc, setDesc] = useState("");
 
 
-	const submit= (e)=>{
+	const submit = (e) => {
 		e.preventDefault();
-		if(!title || !desc){
-			alert("Title or desc not defined!")
+		if (!title || !desc) {
+			alert("Title or description cannot be blank!")
 		}
-		props.addTodo(title,desc)
+		else {
+			props.addTodo(title, desc)
+			setTitle("");
+			setDesc("");
+		}
+
 	}
 
 
@@ -25,12 +30,12 @@ export const Addtodo = (props) => {
 			<Form onSubmit={submit}>
 				<Form.Group className="mb-3" controlId="title">
 					<Form.Label>Todo Title</Form.Label>
-					<Form.Control type="text" value={title} onChange={(e)=>{setTitle(e.target.value)}}placeholder="Enter text here" />
+					<Form.Control type="text" value={title} onChange={(e) => { setTitle(e.target.value) }} placeholder="Enter text here" />
 				</Form.Group>
 
 				<Form.Group className="mb-3" controlId="desc">
 					<Form.Label>Todo Description</Form.Label>
-					<Form.Control type="text" value={desc} onChange={(e)=>{setDesc(e.target.value)}} placeholder="Enter desc. here" />
+					<Form.Control type="text" value={desc} onChange={(e) => { setDesc(e.target.value) }} placeholder="Enter desc. here" />
 				</Form.Group>
 				<Button variant="primary" type="submit" className='btn btn-sm'>
 					Add Todo
